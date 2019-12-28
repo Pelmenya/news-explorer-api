04e6001ec259e375c8b1e5ce74de2fd3b13d6961
Dmitry Lyapin <lyapindm@yandex.ru>
2019-12-28 19:08:15 +0300

    исправил ошибку с роутером для пользователей

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const atriclesRoute = require('./articles');
 const userAuthRoute = require('./userAuth');
-const usersRoute = require('./userAuth');
+const usersRoute = require('./users');
 
 module.exports = { atriclesRoute, userAuthRoute, usersRoute };
