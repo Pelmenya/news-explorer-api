1140060ab381db1890e0a28c84c5b00658fa1b55
Dmitry Lyapin <lyapindm@yandex.ru>
2020-02-07 12:33:59 +0300

    new

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -1,6 +1,6 @@
 const rateLimit = require('express-rate-limit');
 
 module.exports.limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // за 15 минут
-  max: 100, // можно совершить максимум 100 запросов с одного IP
+  windowMs: 5 * 60 * 1000, // за 5 минут
+  max: 1000, // можно совершить максимум 1000 запросов с одного IP
 });
